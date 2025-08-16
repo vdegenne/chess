19bcdb0fc40841d4f18901eac47da72f6ae79572
Valentin Degenne <vdegenne@gmail.com>
2025-08-16 13:07:58 +0200

    0.1.10

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,37 @@
-export function toUnicodeChess(san: string, isWhite = true): string {
-	const pieces: Record<string, string[]> = {
-		K: ['♔', '♚'],
-		Q: ['♕', '♛'],
-		R: ['♖', '♜'],
-		B: ['♗', '♝'],
-		N: ['♘', '♞'],
-		P: ['♙', '♟'], // optional, usually pawns are not written in SAN
-	}
+type Options = {
+	/**
+	 * @default 'w'
+	 */
+	color: 'w' | 'b'
+
+	/**
+	 * If true, will add <span class="unicode"> around unicode
+	 *
+	 * @default false
+	 */
+	spanUnicode: boolean
+}
+
+const pieces: Record<string, string[]> = {
+	K: ['♔', '♚'],
+	Q: ['♕', '♛'],
+	R: ['♖', '♜'],
+	B: ['♗', '♝'],
+	N: ['♘', '♞'],
+	P: ['♙', '♟'], // optional, usually pawns are not written in SAN
+}
+
+export function toUnicodeChess(
+	san: string,
+	options?: Partial<Options>,
+): string {
+	const _options: Options = {color: 'w', spanUnicode: false, ...(options ?? {})}
 
 	// Replace piece letters with unicode symbols
 	return san.replace(/[KQRBNP]/g, (match) => {
-		return isWhite ? pieces[match][0] : pieces[match][1]
+		const unicode = _options.color === 'w' ? pieces[match][0] : pieces[match][1]
+		return _options.spanUnicode
+			? `<span class="unicode">${unicode}</span>`
+			: unicode
 	})
 }
